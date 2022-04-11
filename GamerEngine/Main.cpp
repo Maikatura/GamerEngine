84653173e0d9e1d46aedc0f491a724b00dd05fa1
Zyro / Maikatura <31046190+Maikatura@users.noreply.github.com>
2022-04-11 12:09:32 +0200

    Added a tiangle

--- GamerEngine/Main.cpp
+++ GamerEngine/Main.cpp
@@ -61,12 +61,12 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// GLfloat vertices[] =
-	// {
-	// 	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	// 	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	// 	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-	// };
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
 
 	GLuint VAO, VBO;
 
@@ -76,7 +76,7 @@ int main()
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -109,11 +109,11 @@ int main()
 		ImGui::NewFrame();
 		
 		glUseProgram(shaderProgram);
-		// glBindVertexArray(VAO);
-		// if (drawTriangle)
-		// {
-		// 	glDrawArrays(GL_TRIANGLES, 0, 3);
-		// }
+		glBindVertexArray(VAO);
+		if (drawTriangle)
+		{
+		 	glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
 
 		ImGui::ShowDemoWindow();
 		glUseProgram(shaderProgram);
